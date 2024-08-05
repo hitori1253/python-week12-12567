ba740aa050975e19113b97a4ff72d785caa79118
no <kashi.44738@gmail.com>
2024-08-05 15:10:54 +0700

    first commit

---
+++ python1202.py
@@ -0,0 +1,21 @@
+#2.Assignment Operator
+#As => : =, +=, -=, *=, /=, %=
+a=10
+#แอสซายแวลู่
+b = a
+print("Assignment Value:",b)#=>:10
+#แอด แอ่น แอดซายแวลู๋
+b += a # b=b+a
+print("Assignment Value:",b)#=>:20
+
+b -= a # b=b-a
+print("Assignment Value:",b)#=>:10
+
+b *= a # b=b*a
+print("Assignment Value:",b)#=>:100
+
+b <<= a # b=b<<a
+print("Assignment Value:",b)
+
+b /= a # b=b*a
+print("Assignment Value:",b)
---
+++ python1204.py
@@ -0,0 +1,6 @@
+a=True
+b=False
+result = a&b
+print("Result:",a&b)
+print("Result:",a|b)
+print("Result:",(not a))
